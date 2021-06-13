189131f4defde8489226fa16e9a9ef2d4ca2f1cc
SetMa02 <maxuser99@mail.ru>
2021-06-13 13:37:18 +0700

    Добавьте файлы проекта.

---
+++ Data/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopPr.Data.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string shortDesc { get; set; }
+        public string longDesc { get; set; }
+        public string img { get; set; }
+        public ushort price { get; set; }
+        public bool isFavorite { get; set; }
+        public int availeble { get; set; }
+        public int categoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
